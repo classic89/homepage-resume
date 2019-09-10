9edf412a6baaab71d73dc5dea3b0c8c4bc76b300
classic89 <retrorock8821@gmail.com>
2019-09-09 21:37:30 -0500

    fixed file directory

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('C:/dev/repo/Resume-Website/classic_journey_resume/floating-journey-homepage/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\contact.js"))),
+  "component---src-pages-footer-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\footer.js"))),
+  "component---src-pages-gallery-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\gallery.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\page-2.js"))),
+  "component---src-pages-resume-js": hot(preferDefault(require("C:\\dev\\repo\\Resume-Website\\classic_journey_resume\\floating-journey-homepage\\src\\pages\\resume.js")))
+}
+
